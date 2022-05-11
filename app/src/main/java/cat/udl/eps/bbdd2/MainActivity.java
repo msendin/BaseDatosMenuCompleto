42c4dd2fba895eb862b57c98486ed9d1b48de698
Montse <msendinv@gmail.com>
2022-05-11 12:29:28 +0200

    Feedback from DB operations updated

--- app/src/main/java/cat/udl/eps/bbdd2/MainActivity.java
+++ app/src/main/java/cat/udl/eps/bbdd2/MainActivity.java
@@ -45,113 +45,97 @@ public class MainActivity extends Activity {
  
         db = usdbh.getWritableDatabase();
 		
-		btnInsertar.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				
-				//Recuperamos los valores de los campos de texto
-				String nom = txtNombre.getText().toString();
-				String tel = txtTel.getText().toString();
-				String mail = txtMail.getText().toString();
-				
-				ContentValues nuevoRegistro = new ContentValues();
-				nuevoRegistro.put("nombre", nom);
-				nuevoRegistro.put("telefono", tel);
-				nuevoRegistro.put("email", mail);
-				if (db.insert("Usuarios", null, nuevoRegistro) > 0)
-					showToast(getString(R.string.correctIn));
-				else
-					showToast(getString(R.string.errIn));
-			}
+		btnInsertar.setOnClickListener(v -> {
+			//Recuperamos los valores de los campos de texto
+			String nom = txtNombre.getText().toString();
+			String tel = txtTel.getText().toString();
+			String mail = txtMail.getText().toString();
+
+			ContentValues nuevoRegistro = new ContentValues();
+			nuevoRegistro.put("nombre", nom);
+			nuevoRegistro.put("telefono", tel);
+			nuevoRegistro.put("email", mail);
+			long cont = db.insert("Usuarios", null, nuevoRegistro);
+			if (cont > 0)
+				showToast(getString(R.string.correctIn) + cont);
+			else
+				showToast(getString(R.string.errIn));
 		});
 		
-		btnActualizar.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				
-				//Recuperamos los valores de los campos de texto
-				String nom = txtNombre.getText().toString();
-				String tel = txtTel.getText().toString();
-				String mail = txtMail.getText().toString();
-				
-				String[] args = new String[]{nom};
-				
-				ContentValues valores = new ContentValues();
-				valores.put("telefono", tel);
-				valores.put("email", mail);
-				int cont = db.update("Usuarios", valores, "nombre=?", args);
-				if (cont > 0)
-					showToast(getString(R.string.correctAct)  + Integer.toString(cont));
-				else
-					showToast(getString(R.string.noReg));
-			}
+		btnActualizar.setOnClickListener(v -> {
+			//Recuperamos los valores de los campos de texto
+			String nom = txtNombre.getText().toString();
+			String tel = txtTel.getText().toString();
+			String mail = txtMail.getText().toString();
+
+			String[] args = new String[]{nom};
+
+			ContentValues valores = new ContentValues();
+			valores.put("telefono", tel);
+			valores.put("email", mail);
+			int cont = db.update("Usuarios", valores, "nombre=?", args);
+			if (cont > 0)
+				showToast(getString(R.string.correctAct)  + cont);
+			else
+				showToast(getString(R.string.noReg));
 		});
 		
-		btnEliminar.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				
-				//Recuperamos los valores de los campos de texto
-				String nom = txtNombre.getText().toString();
-				String[] args = new String[]{nom};				
-				int cont = db.delete("Usuarios", "nombre=?", args);
-				if (cont > 0)
-					showToast(getString(R.string.correctEl) + Integer.toString(cont));
-				else
-					showToast(getString(R.string.noReg));
-			}
+		btnEliminar.setOnClickListener(v -> {
+			//Recuperamos los valores de los campos de texto
+			String nom = txtNombre.getText().toString();
+			String[] args = new String[]{nom};
+			int cont = db.delete("Usuarios", "nombre=?", args);
+			if (cont > 0)
+				showToast(getString(R.string.correctEl) + cont);
+			else
+				showToast(getString(R.string.noReg));
 		});
 		
-		btnConsultar.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				
-				//Alternativa 1: metodo rawQuery()
-				Cursor c = db.rawQuery("SELECT nombre, telefono, email FROM Usuarios", null);
-							
-				//Alternativa 2: metodo query()
-				//String[] campos = new String[] {"nombre", "telefono", "email"};
-				//Cursor c = db.query("Usuarios", campos, null, null, null, null, null);
-				
-				//Recorremos los resultados para mostrarlos en pantalla
-				txtResultado.setText("");
-				if (c.moveToFirst()) {
-				     //Recorremos el cursor hasta que no haya mas registros
-				     do {
-				          String nom = c.getString(0);
-				          String tel = c.getString(1);
-				          String mail = c.getString(2);
-				          
-				          txtResultado.append(" " + nom + " - " + tel + " - " + mail + "\n");
-				     } while(c.moveToNext());
-				}
-				else 
-					txtResultado.setText(R.string.tablaV);
-				
+		btnConsultar.setOnClickListener(v -> {
+			//Alternativa 1: metodo rawQuery()
+			Cursor c = db.rawQuery("SELECT nombre, telefono, email FROM Usuarios", null);
+
+			//Alternativa 2: metodo query()
+			//String[] campos = new String[] {"nombre", "telefono", "email"};
+			//Cursor c = db.query("Usuarios", campos, null, null, null, null, null);
+
+			//Recorremos los resultados para mostrarlos en pantalla
+			txtResultado.setText("");
+			if (c.moveToFirst()) {
+				 do {
+					  String nom = c.getString(0);
+					  String tel = c.getString(1);
+					  String mail = c.getString(2);
+					  txtResultado.append(" " + nom + " - " + tel + " - " + mail + "\n");
+				 } while(c.moveToNext());
 			}
+			else
+				txtResultado.setText(R.string.tablaV);
+
 		});
 		
-		btnCons1.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				String[] campos = new String[]{"telefono", "email"};
-				String nom = txtNombre.getText().toString();
-				String[] args = new String[]{nom};
-				Cursor c = db.query("Usuarios", campos, "nombre=?", args, null, null, null);
+		btnCons1.setOnClickListener(v -> {
+			String[] campos = new String[]{"telefono", "email"};
+			String nom = txtNombre.getText().toString();
+			String[] args = new String[]{nom};
+			Cursor c = db.query("Usuarios", campos, "nombre=?", args, null, null, null);
 
-				txtResultado.setText("");
+			txtResultado.setText("");
 
-				//Alternativa 1: metodo rawQuery()
-				//String nom = txtNombre.getText().toString();
-				//String[] args = new String[]{nom};
-				//Cursor c = db.rawQuery("SELECT telefono, email FROM Usuarios WHERE nombre=?", args);
+			//Alternativa 1: metodo rawQuery()
+			//String nom = txtNombre.getText().toString();
+			//String[] args = new String[]{nom};
+			//Cursor c = db.rawQuery("SELECT telefono, email FROM Usuarios WHERE nombre=?", args);
 
-				if (c.moveToFirst()) {
-					//Recorremos el cursor hasta que no haya mas registros
-					do {
-						String tel = c.getString(0);
-						String mail = c.getString(1);
-						txtResultado.append(" " + tel + " - " + mail + "\n");
-					} while (c.moveToNext());
-				} else
-					txtResultado.setText(R.string.noReg);
-			}
-
+			if (c.moveToFirst()) {
+				//Recorremos el cursor hasta que no haya mas registros
+				do {
+					String tel = c.getString(0);
+					String mail = c.getString(1);
+					txtResultado.append(" " + tel + " - " + mail + "\n");
+				} while (c.moveToNext());
+			} else
+				txtResultado.setText(R.string.noReg);
 		});
 	}
 
